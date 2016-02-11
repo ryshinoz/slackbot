daab435aea67ead96a875bafa9d1021cfddb2fb9
Ryosuke Shinozaki <ryshinoz@gmail.com>
2016-02-11 17:47:37 +0900

    Botを落とさせない

--- bot.js
+++ bot.js
@@ -191,3 +191,9 @@ function formatUptime(uptime) {
   uptime = uptime + ' ' + unit;
   return uptime;
 }
+
+// To keep Heroku's free dyno awake
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Ok, dyno is awake.');
+}).listen(process.env.PORT || 5000);
